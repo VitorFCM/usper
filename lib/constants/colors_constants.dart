8de499fa430031c2c29d14360b66b61f57059cf2
VitorFCM <vitor.carreramiguel@gmail.com>
2023-09-04 17:58:05 -0300

    Creating a basic home screen file and a color constants file

---
+++ lib/constants/colors_constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const yellow = Color(0xFFFCB421);
+const lighterBlue = Color(0xFF64C4D2);
+const blue = Color(0xFF1094AB);
+const white = Color(0xFFfcfcff);
+const black = Color(0x00000000);
